ae34e1ff08a52bcf35a3a8967383e44460994139
Christian Fleury <chfleurysiq@gmail.com>
2021-10-03 17:04:11 -0300

    login page

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,1 +1,14 @@
-static const BASE_URL: '207.128.2.29:3333';
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const BASE_URL = '207.128.2.29:3333';
+
+  static const title = 'ExchBook';
+
+  static const login = 'Entrar';
+  static const register = 'Registrar';
+  static const dontHaveAccount = 'Ainda não tem uma conta? Clique aqui!';
+
+  static const primaryColor = const Color(0xFF295387);
+  static const secondaryColor = const Color.fromRGBO(230, 57, 70, 1);
+}
---
+++ lib/views/login_page.dart
@@ -0,0 +1,182 @@
+import 'package:exch_book/util/constants.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _senha = '';
+
+  _submit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // _loginBloc.add(Login(email: _email, senha: _senha));
+    }
+  }
+
+  @override
+  void initState() {
+    // _loginBloc = LoginBloc(repository);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Constants.primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset('images/logo.png'),
+              height: size.height * 0.1,
+            ),
+            Text(
+              'ExchBook',
+              style: TextStyle(
+                  fontFamily: 'SIMPLIFICA Typeface',
+                  fontSize: 30,
+                  color: Colors.white),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 10,
+                    ),
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      validator: (input) => (input!.length > 5 &&
+                              input.trim().length > 0 &&
+                              input.contains('@'))
+                          ? null
+                          : 'Por favor insira um email válido',
+                      decoration: InputDecoration(
+                          labelText: 'E-mail',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          )),
+                      onSaved: (input) => _email = input!,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 10,
+                    ),
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      cursorColor: Colors.pink,
+                      obscureText: true,
+                      validator: (input) => input!.length < 6
+                          ? 'A senha deve conter pelo menos 6 caracteres'
+                          : null,
+                      decoration: InputDecoration(
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(color: Colors.white)),
+                      onSaved: (input) => _senha = input!,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: size.width * 0.8,
+                    height: size.height * 0.08,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Constants.secondaryColor),
+                        onPressed: () => {},
+                        child: Text(
+                          Constants.login,
+                          style: TextStyle(fontSize: 20),
+                        )),
+                  ), // BlocListener<LoginBloc, LoginState>(
+                  //   bloc: _loginBloc,
+                  //   listener: (context, state) {
+                  //     if (state is LoginLoaded) {
+                  //       Navigator.of(context)
+                  //           .pushReplacementNamed(HomeScreen.id);
+                  //     } else if (state is LoginError) {
+                  //       print('Usuario ou senha invalidos');
+                  //     }
+                  //   },
+                  //   child: BlocBuilder<LoginBloc, LoginState>(
+                  //       bloc: _loginBloc,
+                  //       builder: (context, state) {
+                  //         if (state is LoginLoading) {
+                  //           return Center(
+                  //             child: LinearProgressIndicator(),
+                  //           );
+                  //         }
+                  //         return GradientButton(
+                  //             label: 'Entrar', onPressed: _submit);
+                  //       }),
+                  // ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                      onPressed: () => {},
+                      child: Text(
+                        Constants.dontHaveAccount,
+                        style: TextStyle(color: Colors.white),
+                      ))
+                  // Row(
+                  //   crossAxisAlignment: CrossAxisAlignment.center,
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: <Widget>[
+                  //     Text('Ainda não possui uma conta?'),
+                  //     // _buildRegisterButton(),
+                  //   ],
+                  // ),
+                ],
+                mainAxisSize: MainAxisSize.min,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// _buildRegisterButton() {
+//   return Builder(builder: (context) {
+//     return TextButton(
+//       onPressed: () async {
+//         var response = await Navigator.of(context).pushNamed(RegisterScreen.id);
+//         if (response) {
+//           Scaffold.of(context).showSnackBar(SnackBar(
+//             content: Text(
+//               'Conta criada com Sucesso',
+//               style: TextStyle(
+//                 fontSize: 18,
+//               ),
+//             ),
+//             backgroundColor: Colors.green,
+//           ));
+//         }
+//       },
+//       child: Text(
+//         'Registre-se',
+//         style: TextStyle(
+//           color: Colors.pink,
+//         ),
+//       ),
+//     );
+//   });
+// }
